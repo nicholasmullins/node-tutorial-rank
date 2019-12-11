cbcb7107f13c283c5ca79faa815c0a79da8b61ea
Home <home@Homes-MacBook.local>
2019-12-11 12:49:28 -0700

    commit before push to heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
-
 // Calling connectDB to connect my database on MongoDB atlas
 
 const app = express();
@@ -12,22 +11,26 @@ connectDB();
 
 // Middleware - Do not need to use BodyParser because it is included with express now. Instead we are putting the code below:
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('API Running'));
+// MAIN SERVER ROUTE
+
+app.get('/', (req, res) => res.send('API running'));
 
 // CONTROLLERS - DEFINING ROUTES
 
-const usersController = require('./routes/api/users');
-app.use('/api/users', usersController);
+const usersController = require('./routes/api/users')
+app.use('/api/users', usersController); 
 
-const profilesController = require('./routes/api/profile');
+const profilesController = require('./routes/api/profile')
 app.use('/api/profile', profilesController);
 
-const authController = require('./routes/api/auth');
-app.use('/api/auth', authController);
+const authController = require('./routes/api/auth')
+app.use('/api/auth', authController); 
 
-const videosController = require('./routes/api/videos');
-app.use('/api/videos', videosController);
+const videosController = require('./routes/api/videos')
+app.use('/api/videos', videosController); 
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
